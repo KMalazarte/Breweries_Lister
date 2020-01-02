d604f893734cd7bf1019c309e50d628448506ec8
Kevin Malazarte <Kevin.Malazarte@gmail.com>
2020-01-02 14:50:00 -0500

    breweriesList component created and connected to App

---
+++ src/components/breweriesList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BreweriesList extends Component {
+
+  render() {
+    return (
+      <div>Hello from BreweriesList!</div>
+    )
+  }
+
+}
+
+export default BreweriesList;
