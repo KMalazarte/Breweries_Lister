c987f160a31825931c6ad6218329de4080667d2e
Kevin Malazarte <Kevin.Malazarte@gmail.com>
2020-01-02 15:35:37 -0500

    ATL breweries are being fetched successfully

--- src/components/breweriesList.js
+++ src/components/breweriesList.js
@@ -1,10 +1,35 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 
 class BreweriesList extends Component {
 
+  state={
+    breweries: [],
+    clicked: false
+  }
+
+  componentDidMount() {
+    fetch('https://api.openbrewerydb.org/breweries?by_city=Atlanta')
+      .then( resp => resp.json() )
+      .then( breweries => {
+        console.log("%c breweries array",'color: firebrick', breweries);
+        this.setState({
+          breweries: breweries
+        })
+      })
+  }
+
+  // .then(moviesArr =>  {
+    //     // console.log(moviesArr.movies);
+    //     this.setState({
+    //     movies: moviesArr.movies
+    //     })
+
   render() {
+    console.log("%c breweries state",'color: blue', this.state.breweries);
     return (
-      <div>Hello from BreweriesList!</div>
+      <Fragment>
+        <div>Hello from breweries list</div>
+      </Fragment>
     )
   }
 
