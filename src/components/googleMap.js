819f976cf2142ab8b8694c8c5d8cdfe41c09bb95
Kevin Malazarte <Kevin.Malazarte@gmail.com>
2020-01-06 16:34:06 -0500

    added styling to details page, fixed size of beer marker on map

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import { Header } from 'semantic-ui-react';
 import '../App.css'
 
 class GoogleMap extends Component {
 
   render() {
 
-    const Marker = ({ text }) => <span role="img">"🍺"</span>
+    const Marker = ({ text }) => <span id="marker" role="img">{text}</span>
 
     // const handleApiLoaded = (map, maps) => {
     //    //use map and maps objects
@@ -14,43 +15,40 @@ class GoogleMap extends Component {
 
     let center = {}
 
+    const zoom = 14
+
+    console.log("%c google map props",'color: orange', this.props);
+
     if (this.props.lat && this.props.lng) {
       center = {
         lat:parseFloat(this.props.lat),
         lng:parseFloat(this.props.lng)
       }
+      return (
+        <div id="map">
+          <GoogleMapReact
+            bootstrapURLKeys={{
+             key: process.env.REACT_APP_GOOGLE_API_KEY,
+             language: 'US',
+             region: 'US'
+            }}
+            defaultCenter={center}
+            defaultZoom={zoom}
+          >
+          <Marker
+            lat={this.props.lat}
+            lng={this.props.lng}
+            text="🍺"
+          />
+          </GoogleMapReact>
+
+        </div>
+      )
     } else {
-      center = {
-        lat:33.750160,
-        lng:-84.389113
-      }
+      return (
+        <Header content="Sorry, map not available"/>
+      )
     }
-
-    const zoom = 14
-
-    console.log("%c google map props",'color: orange', this.props);
-
-    return (
-      // Important! Always set the container height explicitly
-      <div id="map">
-        <GoogleMapReact
-          bootstrapURLKeys={{
-           key: process.env.REACT_APP_GOOGLE_API_KEY,
-           language: 'US',
-           region: 'US'
-          }}
-          defaultCenter={center}
-          defaultZoom={zoom}
-        >
-        <Marker
-          lat={this.props.lat}
-          lng={this.props.lng}
-          text="🍺"
-        />
-        </GoogleMapReact>
-
-      </div>
-    )
   }
 }
 
